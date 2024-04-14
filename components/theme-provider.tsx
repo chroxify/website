ac9f434ea11d9291f907e6cb1bb3859d6dbd4537
chroxify <chvtodorov@gmail.com>
2024-04-14 14:49:02 +0200

    wip

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,6 +4,8 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 
+export const revalidate = 0;
+
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
