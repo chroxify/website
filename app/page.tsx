2aa63716125e4e0e344e194b04beda27587a555b
Christo Todorov <chroxify@gmail.com>
2024-12-26 19:04:22 +0100

    feat: reset app

--- app/page.tsx
+++ app/page.tsx
@@ -1,115 +1,101 @@
-import { cn } from "@/lib/utils";
-import { Newsreader } from "next/font/google";
-import ContentLink from "@/components/content-link";
-import Social from "@/components/social";
-import { allCrafts } from "@/.contentlayer/generated";
-
-const newsreader = Newsreader({
-  subsets: ["latin"],
-  style: ["normal", "italic"],
-});
-
-const PROJECTS = [
-  {
-    name: "Haptic",
-    url: "https://github.com/chroxify/haptic",
-    description: "Open source markdown editor.",
-    year: "2024",
-  },
-  {
-    name: "Feedbase",
-    url: "https://feedbase.app",
-    description: "Collect feedback & announce product updates.",
-    year: "2023",
-  },
-  {
-    name: "Supafork",
-    url: "https://supafork.com",
-    description: "Fork supabase projects in a click.",
-    year: "2023",
-  },
-  {
-    name: "Namegpt",
-    url: "https://github.com/chroxify/namegpt",
-    description: "Let AI name your next project.",
-    year: "2023",
-  },
-  {
-    name: "Sherlock Web",
-    url: "https://github.com/chroxify/sherlock-web",
-    description: "Search for usernames across social networks.",
-    year: "2021",
-  },
-];
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      {/* About */}
-      <div className="space-y-2 w-full animate-enter delay-75">
-        <h2 className={cn(newsreader.className, "text-secondary-foreground")}>
-          About me
-        </h2>
-        <p>
-          Building things on the web since 2019. I&apos;m especially passionate
-          about well executed design, attention to detail, and the intersection
-          of code and art.
-        </p>
-      </div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              app/page.tsx
+            </code>
+            .
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
 
-      {/* Projects */}
-      <div className="space-y-2 w-full animate-enter delay-100">
-        <h2 className={cn(newsreader.className, "text-secondary-foreground")}>
-          Projects
-        </h2>
-
-        <ul className="space-y-2 w-full">
-          {PROJECTS.map((project) => (
-            <ContentLink
-              key={project.name}
-              name={project.name}
-              url={project.url}
-              description={project.description}
-              year={project.year}
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
             />
-          ))}
-        </ul>
-      </div>
-
-      {/* Crafts */}
-      <div className="space-y-2 w-full animate-enter delay-125">
-        <h2 className={cn(newsreader.className, "text-secondary-foreground")}>
-          Crafts
-        </h2>
-        <ul className="space-y-2 w-full">
-          {allCrafts
-            .sort(
-              (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-            )
-            .map((craft) => (
-              <ContentLink
-                key={craft._raw.flattenedPath}
-                name={craft.title}
-                url={`/crafts/${craft._raw.flattenedPath}`}
-                description={craft.description}
-                keepTab
-              />
-            ))}
-        </ul>
-      </div>
-
-      {/* Socials */}
-      <div className="space-y-2 w-full animate-enter delay-150">
-        <h2 className={cn(newsreader.className, "text-secondary-foreground")}>
-          Socials
-        </h2>
-        <div className="flex items-center justify-start gap-2">
-          <Social name="Mail" url="mailto:chroxify@gmail.com" />
-          <Social name="Twitter" url="https://twitter.com/chroxify" />
-          <Social name="GitHub" url="https://github.com/chroxify" />
-          <Social name="LinkedIn" url="https://linkedin.com/in/chroxify" />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
         </div>
-      </div>
-    </>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
   );
 }
